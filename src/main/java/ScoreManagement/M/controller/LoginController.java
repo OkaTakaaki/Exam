38044827ed9168b102a87ddb1bd30bcb86241848
Oka <fko2347014@stu.o-hara.ac.jp>
2024-05-15 15:30:26 +0900

    komitto

--- src/main/java/ScoreManagement/M/controller/LoginController.java
+++ src/main/java/ScoreManagement/M/controller/LoginController.java
@@ -1,9 +1,15 @@
 package ScoreManagement.M.controller;
 
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @Controller
 
@@ -21,5 +27,14 @@ public class LoginController {
 		return "redirect:top";
 
 	}
+	
+	@RequestMapping("/logout/")
+    public String logout(HttpServletRequest request, HttpServletResponse response) {
+        org.springframework.security.core.Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null) {
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+        }
+        return "logout"; // Redirect to the home page after logout
+    }
 
 }
